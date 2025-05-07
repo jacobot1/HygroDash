6f3ca073efc66c10e82360048687eb9c3ca37f5a
jacobot1 <155650471+jacobot1@users.noreply.github.com>
2025-05-06 23:45:43 -0500

    Add project files.

---
+++ HygroDash/Patches/BlobAIPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace HygroDash.Patches
+{
+    [HarmonyPatch(typeof(BlobAI))]
+    internal class BlobAIPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void SpeedChangePatch(BlobAI __instance)
+        {
+            __instance.agent.speed *= HygroDashMod.configSpeedMultiplier.Value;
+        }
+    }
+}
---
+++ HygroDash/Plugin.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Logging;
+using HygroDash.Patches;
+using BepInEx.Configuration;
+
+namespace HygroDash
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class HygroDashMod : BaseUnityPlugin
+    {
+        // Mod metadata
+        public const string modGUID = "jacobot5.HygroDash";
+        public const string modName = "HygroDash";
+        public const string modVersion = "1.0.0";
+
+        // Initalize Harmony
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        // Create static instance
+        private static HygroDashMod Instance;
+
+        // Configuration
+        public static ConfigEntry<float> configSpeedMultiplier;
+
+        // Initialize logging
+        public static ManualLogSource mls;
+
+        private void Awake()
+        {
+            // Ensure static instance
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            // Send alive message
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("HygroDash has awoken.");
+
+            // Bind configuration
+            configSpeedMultiplier = Config.Bind("General.Toggles",
+                                                "SpeedMultiplier",
+                                                10f,
+                                                "How much to multiply Hygrodere Speed by. 1 is normal speed, defaults to 10.");
+
+            // Do the patching
+            harmony.PatchAll(typeof(HygroDashMod));
+            harmony.PatchAll(typeof(BlobAIPatch));
+        }
+    }
+}
